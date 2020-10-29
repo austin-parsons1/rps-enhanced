5ca5b52f899f602f7072a1583b73e0ecc28e0a22
Austin Parsons <austin.parsons813@gmail.com>
2020-10-29 15:50:45 -0400

    Completed Percent dispays and Leaderboard name, working on test

--- RPS-web/src/app/leaderboard/leaderboard.component.ts
+++ RPS-web/src/app/leaderboard/leaderboard.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { PlayerStat, GameRecord } from '../game/game';
-import { GameGateway } from '../game/game.gateway';
+import { Component, OnInit } from "@angular/core";
+import { PlayerStat, GameRecord } from "../game/game";
+import { GameGateway } from "../game/game.gateway";
 
 @Component({
-  selector: 'app-leaderboard',
-  templateUrl: './leaderboard.component.html',
-  styleUrls: ['./leaderboard.component.css']
+  selector: "app-leaderboard",
+  templateUrl: "./leaderboard.component.html",
+  styleUrls: ["./leaderboard.component.css"],
 })
 export class LeaderboardComponent implements OnInit {
-
   playerStats: PlayerStat[] = [];
   gameRecords: GameRecord[] = [];
   selectedPlayer = -1;
+  selectedPlayerStats: PlayerStat;
 
   constructor(private gameGateway: GameGateway) {
     this.playerStats = [];
@@ -19,42 +19,45 @@ export class LeaderboardComponent implements OnInit {
   }
 
   ngOnInit() {
-    
     this.getPlayerStats();
   }
 
-  showPlayer( playerId: number ){
-    console.log('Click - Player: ', playerId );
+  showPlayer(playerId: number) {
+    console.log("Click - Player: ", playerId);
     this.selectedPlayer = playerId;
-    if( playerId === -1 ) {
+    if (playerId === -1) {
       this.getPlayerStats();
     } else {
-      this.getPlayerStats();
+      this.getPlayerStatsById(playerId);
       this.getGameRecords();
     }
-
   }
-
+  getPlayerStatsById(id: number) {
+    this.selectedPlayerStats = this.playerStats.filter(
+      (x) => x.player.id === id
+    )[0];
+  }
   getPlayerStats() {
     this.playerStats = [];
-    this.gameGateway.getPlayerStats().subscribe(returnedPlayerStats => {
-      for(let i = 0; i < returnedPlayerStats.length; i++) {
+    this.gameGateway.getPlayerStats().subscribe((returnedPlayerStats) => {
+      for (let i = 0; i < returnedPlayerStats.length; i++) {
         this.playerStats.push(returnedPlayerStats[i]);
       }
       // this.playerList = this.playerList.sort((a,b) => a.name.localeCompare(b.name));
-      console.log('got player Stats', this.playerStats);
+      console.log("got player Stats", this.playerStats);
     });
   }
 
-  getGameRecords(){
+  getGameRecords() {
     this.gameRecords = [];
-    this.gameGateway.getPlayerGameRecords( this.selectedPlayer).subscribe(returnedGameRecords => {
-      for(let i = 0; i < returnedGameRecords.length; i++) {
-        this.gameRecords.push(returnedGameRecords[i]);
-      }
-      // this.playerList = this.playerList.sort((a,b) => a.name.localeCompare(b.name));
-      console.log('got player game records', this.gameRecords);
-    });
+    this.gameGateway
+      .getPlayerGameRecords(this.selectedPlayer)
+      .subscribe((returnedGameRecords) => {
+        for (let i = 0; i < returnedGameRecords.length; i++) {
+          this.gameRecords.push(returnedGameRecords[i]);
+        }
+        // this.playerList = this.playerList.sort((a,b) => a.name.localeCompare(b.name));
+        console.log("got player game records", this.gameRecords);
+      });
   }
-
 }
